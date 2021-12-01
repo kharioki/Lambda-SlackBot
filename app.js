72d241497827baceb3aaab9c0ebf49edae1359d4
tony <antolisah@gmail.com>
2021-12-01 23:13:00 +0300

    initialize a slack app

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  // Log to console
+  console.log('⚡️ Bolt app is running!');
+})();
