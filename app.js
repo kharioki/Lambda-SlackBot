dc79d2fec1cbae8cc41e8b13d65e90da4b31b8e6
tony <antolisah@gmail.com>
2021-12-02 00:14:31 +0300

    AWS lambda and serverless offline config

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
-const { App } = require('@slack/bolt');
+const { App, AwsLambdaReceiver } = require('@slack/bolt');
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+// custom receiver
+const awsLambdaReceiver = new AwsLambdaReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver: awsLambdaReceiver,
+  processBeforeResponse: true
+});
 
-  // Log to console
-  console.log('⚡️ Bolt app is running!');
-})();
+app.message('hello', async ({ message, say }) => {
+  await say(`Hello <@${message.user}>!`);
+});
+
+module.exports.handler = async (event, context, callback) => {
+  const handler = await awsLambdaReceiver.start();
+  return handler(event, context, callback);
+};
